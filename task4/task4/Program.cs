40031f6d3047d8bd071f3dc2e5b8957c3de058d0
AnikinaDaria <76163884+AnikinaDaria@users.noreply.github.com>
2023-12-18 00:38:48 +0400

    Add files via upload

---
+++ task4/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до файла");
+            string path = Console.ReadLine();
+            var listNums = new List<int>();
+            using (StreamReader sr = new(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    listNums.Add(int.Parse(line));
+                }
+            }
+            listNums.Sort();
+            var mid = listNums.Count / 2;
+            var result = 0;
+            foreach (var num in listNums)
+            {
+                result += Math.Abs(listNums[mid] - num);
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
