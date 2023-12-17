d5f9c20bd155788bef063b86c1e0f5eba4cb4a8b
AnikinaDaria <76163884+AnikinaDaria@users.noreply.github.com>
2023-12-18 00:28:58 +0400

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            var arr = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = i + 1;
+            int index = 0;
+            do{ 
+                Console.Write(arr[index]);
+                index = (index + m - 1) % n;
+            } while (index != 0);
+            Console.ReadKey();
+        }
+    }
+}
